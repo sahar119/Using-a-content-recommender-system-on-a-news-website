fbb137681e85778e6d789a7a7fc70f56c61cff19
Sahar Koolaee <108146301+sahar119@users.noreply.github.com>
2023-11-11 03:18:40 +0100

    Add files via upload

---
+++ About_project_ℹ️.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title(" Doomscrolling Project 💡")
+st.markdown("Dear participants, This project aims to determine the best strategies to prevent doomscrolling among people with high online content consumption. This crime news website simulates a doomscrolling web page where users get stuck in a negative content loop. Then we will ask you for your opinions and feedback. Thank you for your time. Sahar Kh.Koolaee")
+
+
+
+
+
